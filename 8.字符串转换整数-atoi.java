a5e5950fe681d58c5405cf52779f9e6b99d065e8
lettino <fm518812@163.com>
2020-05-08 18:33:04 +0800

    新增 字符串转换整数

---
+++ 8.字符串转换整数-atoi.java
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=8 lang=java
+ *
+ * [8] 字符串转换整数 (atoi)
+ */
+
+// @lc code=start
+class Solution {
+    public int myAtoi(String str) {
+        System.out.print("Hello World");
+        return 0;
+    }
+}
+// @lc code=end
+
