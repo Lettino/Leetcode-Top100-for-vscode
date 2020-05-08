8c258c330c08ade682a94adb0da7cab04b7abe23
lettino <fm518812@163.com>
2020-05-08 20:34:30 +0800

    update 8.atoi

--- 8.字符串转换整数-atoi.java
+++ 8.字符串转换整数-atoi.java
@@ -7,9 +7,41 @@
 // @lc code=start
 class Solution {
     public int myAtoi(String str) {
-        System.out.print("Hello World");
-        return 0;
+        int n = str.length();
+        int i = 0;
+        while (i < n && str.charAt(i) == ' ') {
+            i++; // 如果有空格的话就跳过
+        }
+        if (i == n || !((str.charAt(i) == '+') || (str.charAt(i) == '-')
+                || (str.charAt(i) >= '0' && str.charAt(i) <= '9'))) {
+            // 如果第i个字符不是最后一个 或者该字符不是+ - 或者数字
+            return 0;
+        }
+        StringBuilder sb = new StringBuilder();
+        if (str.charAt(i) == '-') {
+            sb.append('-');
+            i++;
+        } else if (str.charAt(i) == '+') {
+            i++;
+        }
+        if (i == n || !(str.charAt(i) >= '0' && str.charAt(i) <= '9')) {
+            return 0;
+        }
+        while (i < n && str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+            sb.append(str.charAt(i));
+            i++;
+        }
+     
+        try {
+            return Integer.valueOf(sb.toString());
+        } catch (Exception e) {
+            if (sb.substring(0, 1).equals("-")) {
+                return Integer.MIN_VALUE;
+            } else {
+                return Integer.MAX_VALUE;
+            }
+        }
+       
     }
 }
 // @lc code=end
-
